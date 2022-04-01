757f26eb9410aedcfb1c9d0c6e8e205e822a4b3f
Victor <vicle18@student.sdu.dk>
2022-04-01 15:01:57 +0200

    her aske

---
+++ esp8266_count_mqtt/esp8266_count_mqtt.ino
@@ -0,0 +1,179 @@
+
+
+// Linux for Embedded Objects 1
+// University of Southern Denmark
+
+// 2022-03-24, Kjeld Jensen, First version
+
+// Configuration
+#define WIFI_SSID       "LEO1_TEAM_06"
+#define WIFI_PASSWORD    "embeddedlinux"
+
+#define MQTT_SERVER      "192.168.10.1"
+#define MQTT_SERVERPORT  1883 
+#define MQTT_USERNAME    "my_user"
+#define MQTT_KEY         "bendevictor"
+#define MQTT_TOPIC       "/count"  
+#define VOLTAGE_TOPIC    "/voltage"
+
+// wifi
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+ESP8266WiFiMulti WiFiMulti;
+const uint32_t conn_tout_ms = 5000;
+
+// counter
+#define GPIO_INTERRUPT_PIN 4
+#define DEBOUNCE_TIME 100 
+volatile unsigned long count_prev_time;
+volatile unsigned long count;
+
+// mqtt
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+WiFiClient wifi_client;
+Adafruit_MQTT_Client mqtt(&wifi_client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
+Adafruit_MQTT_Publish count_mqtt_publish = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME MQTT_TOPIC);
+Adafruit_MQTT_Publish voltage_mqtt_publish = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME VOLTAGE_TOPIC);
+
+
+// publish
+#define PUBLISH_INTERVAL 15000
+unsigned long prev_post_time;
+
+// debug
+#define DEBUG_INTERVAL 2000
+unsigned long prev_debug_time;
+
+ICACHE_RAM_ATTR void count_isr()
+{
+  if (count_prev_time + DEBOUNCE_TIME < millis() || count_prev_time > millis())
+  {
+    count_prev_time = millis(); 
+    count++;
+  }
+}
+
+void debug(const char *s)
+{
+  Serial.print (millis());
+  Serial.print (" ");
+  Serial.println(s);
+}
+
+void mqtt_connect()
+{
+  int8_t ret;
+
+  // Stop if already connected.
+  if (! mqtt.connected())
+  {
+    debug("Connecting to MQTT... ");
+    while ((ret = mqtt.connect()) != 0)
+    { // connect will return 0 for connected
+         Serial.println(mqtt.connectErrorString(ret));
+         debug("Retrying MQTT connection in 5 seconds...");
+         mqtt.disconnect();
+         delay(5000);  // wait 5 seconds
+    }
+    debug("MQTT Connected");
+  }
+}
+
+void print_wifi_status()
+{
+  Serial.print (millis());
+  Serial.print(" WiFi connected: ");
+  Serial.print(WiFi.SSID());
+  Serial.print(" ");
+  Serial.print(WiFi.localIP());
+  Serial.print(" RSSI: ");
+  Serial.print(WiFi.RSSI());
+  Serial.println(" dBm");
+}
+
+void setup()
+{
+  // count
+  count_prev_time = millis();
+  count = 0;
+  pinMode(GPIO_INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(GPIO_INTERRUPT_PIN), count_isr, RISING);
+
+  // serial
+  Serial.begin(115200);
+  delay(10);
+  debug("Boot");
+
+  //Voltage serial
+  
+
+  // wifi
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  if(WiFiMulti.run(conn_tout_ms) == WL_CONNECTED)
+  {
+    print_wifi_status();
+  }
+  else
+  {
+    debug("Unable to connect");
+  }
+}
+
+void publish_data()
+{
+  char payload[10];
+  sprintf (payload, "%ld", count);
+  count = 0;
+  Serial.print(millis());
+  Serial.print(" Publishing: ");
+  Serial.println(payload);
+  int voltage_Value = analogRead(A0);
+
+
+  Serial.print(millis());
+  Serial.println(" Connecting...");
+  if((WiFiMulti.run(conn_tout_ms) == WL_CONNECTED))
+  {
+    print_wifi_status();
+  
+    mqtt_connect();
+    Serial.println("connect success");
+    if (! count_mqtt_publish.publish(payload))
+    {
+      debug("MQTT failed");
+    }
+    else
+    {
+      debug("MQTT ok");
+    }
+    if(!voltage_mqtt_publish.publish(voltage_Value)){
+      debug("MQTT failed");
+    }
+    else
+    {
+      debug("MQTT ok");
+    }
+  }
+}
+void loop()
+{
+    if (millis() - prev_post_time >= PUBLISH_INTERVAL)
+    {
+      prev_post_time = millis();
+      publish_data();
+    }
+   
+    if (millis() - prev_debug_time >= DEBUG_INTERVAL)
+    {
+      prev_debug_time = millis();
+      Serial.print(millis());
+      Serial.print(" ");
+      Serial.println(count);
+      //Serial.print(" Publishing Serial Voltage: ");
+      //int potValue = analogRead(A0);
+      //Serial.println(potValue); 
+    }
+}
