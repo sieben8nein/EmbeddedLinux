e6f47a3beea2dee5544634200f05b1b82d258bcf
Unknown <aske.lorenz@gmail.com>
2022-04-29 15:24:31 +0200

    Added python mqtt subscriber

---
+++ python_mqtt_listener/mqttSubscribe-and_rulecheck.py
@@ -0,0 +1,56 @@
+from paho.mqtt import client as mqtt_client
+
+broker = 'tcp://localhost'
+port = 1883
+topic1 = "temp"
+topic2 = "humidity"
+topic3 = "co2"
+client_id = 'python-mqtt-rulechecker'
+username = 'my_user'
+password = 'bendevictor'
+
+""" def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client """
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt_client, topic):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client, topic1)
+    subscribe(client, topic2)
+    subscribe(client, topic3)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
