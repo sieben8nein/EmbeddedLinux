18a7d538d65894cb0c7b30be591479f91e29d2bb
Unknown <aske.lorenz@gmail.com>
2022-04-29 16:00:00 +0200

    Added publish function

--- python_mqtt_listener/mqttSubscribe-and_rulecheck.py
+++ python_mqtt_listener/mqttSubscribe-and_rulecheck.py
@@ -5,22 +5,10 @@ port = 1883
 topic1 = "temp"
 topic2 = "humidity"
 topic3 = "co2"
+pubTopic = "actuators"
 client_id = 'python-mqtt-rulechecker'
 username = 'my_user'
 password = 'bendevictor'
-
-""" def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
-    client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client """
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -36,18 +24,51 @@ def connect_mqtt() -> mqtt_client:
     return client
 
 
-def subscribe(client: mqtt_client, topic1, topic2, topic3):
+def subscribe(client: mqtt_client, topic1):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        ruleCheck(msg.payload.decode(), msg.topic, client)
 
-    client.subscribe([topic1,topic2,topic3])
+    client.subscribe(topic1)
     client.on_message = on_message
 
+def publish(client, message):
+    msg = message
+    result = client.publish(pubTopic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{pubTopic}`")
+    else:
+        print(f"Failed to send message to topic {pubTopic}")
+    
+
+def ruleCheck(value, topic, client):
+    if topic == "temp":
+        if value > 25:
+            publish(client, ["fan", "open"])
+        else:
+            publish(client, ["fan", "close"])
+        
+    elif topic == "humidity":
+        if value > 30:
+            publish(client, ["dehumidifyer", "open"])
+        else:
+            publish(client, ["dehumidifyer", "close"])
+    elif topic == "co2":
+        if value > 1200:
+            publish(client, ["window", "open"])
+        else:
+            publish(client, ["window", "close"])
+    return
 
 def run():
     client = connect_mqtt()
-    subscribe(client, topic1,topic2,topic3)
+    subscribe(client, topic1)
+    subscribe(client, topic2)
+    subscribe(client, topic3)
     client.loop_forever()
+    
 
 
 if __name__ == '__main__':
